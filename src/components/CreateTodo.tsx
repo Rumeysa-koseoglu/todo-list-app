df714511f897350258f1fd725fb188467a8fc1cd
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-06-30 13:53:32 +0300

    feat: add Redux logic for creating todos

--- src/components/CreateTodo.tsx
+++ src/components/CreateTodo.tsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createTodo } from "../redux/TodoSlice";
+import type { TodoType } from "../types/Types";
 
 function CreateTodo() {
+  const dispatch = useDispatch();
+
+  const [newTodo, setNewTodo] = useState<string>("");
+
+  const handleCreateTodo = () => {
+    if (newTodo.trim().length == 0) {
+      alert("please enter a task!");
+      return;
+    }
+
+    const payload: TodoType = {
+      id: Math.floor(Math.random() * 999999999),
+      content: newTodo,
+    };
+    dispatch(createTodo(payload));
+    setNewTodo("");
+  };
+
   return (
     <div className="create-todo-container">
-      <input className="todo-input" type="text" placeholder="enter a task.." />
-      <button className="create-button">create</button>
+      <input
+        value={newTodo}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setNewTodo(e.target.value)
+        }
+        className="todo-input"
+        type="text"
+        placeholder="enter a task.."
+      />
+      <button onClick={handleCreateTodo} className="create-button">
+        create
+      </button>
     </div>
   );
 }
