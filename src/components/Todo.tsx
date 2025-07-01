eb078bfea116167af18267f1c202d3c77c5b2be9
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-07-01 14:21:06 +0300

    feat: implement todo deletion

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -4,6 +4,8 @@ import { CiCircleCheck } from "react-icons/ci";
 import { CiEdit } from "react-icons/ci";
 import "../css/Todo.css";
 import type { TodoType } from "../types/Types";
+import { useDispatch } from "react-redux";
+import { removeTodoById } from "../redux/TodoSlice";
 
 interface TodoProps {
   todoProps: TodoType;
@@ -11,11 +13,18 @@ interface TodoProps {
 
 function Todo({ todoProps }: TodoProps) {
   const { id, content } = todoProps;
+
+  const dispatch = useDispatch();
+
+  const handleRemoveTodo = () => {
+    dispatch(removeTodoById(id));
+  };
   return (
     <div className="todos-container">
       <div>{content}</div>
       <div>
         <IoIosRemoveCircleOutline
+          onClick={handleRemoveTodo}
           className="icons"
           style={{ marginRight: "8px" }}
         />
